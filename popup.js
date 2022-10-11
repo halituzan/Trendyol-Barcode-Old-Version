dfe303485900cc10086d05b2f440ec54f65607cb
Halit Uzan <halit.uzan@gmail.com>
2022-10-11 15:05:26 +0300

    undefined hatası giderildi

--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
-window.addEventListener('load',function(e) {
-  chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null,{
-    file : 'script.js',
+window.addEventListener("load", function (e) {
+  chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+    file: "script.js",
   });
 });
 
 chrome.runtime.onMessage.addListener(function (val) {
   document.getElementById("txtBarkod").innerHTML = document.title;
   document.getElementById("textAreaID").innerHTML = val[1];
+  if (!val[2][0].barcode) {
+    document.getElementById(
+      "re-Load"
+    ).innerHTML = `Barkod undefined hatası veriyorsa lütfen <button><a href="https://trendyol.com${val[3].url}" target="_blank">Yenile</a></button> butonuna tıklayın ve tekrar deneyin`;
+  }
 });
-
-
--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 function kods() {
-    let komut = document.querySelectorAll("script[type='application/javascript']");
-    let komutArr = Array.from(komut);
-    let indexApp = 0;
-    for (i = 0; i < komutArr.length; i++) {
-        if (komutArr[i].innerText.includes("__PRODUCT_DETAIL_APP_INITIAL_STATE__")) {
-            indexApp = i;
-        }
+  let komut = document.querySelectorAll(
+    "script[type='application/javascript']"
+  );
+  let komutArr = Array.from(komut);
+  let indexApp = 0;
+  for (i = 0; i < komutArr.length; i++) {
+    if (
+      komutArr[i].innerText.includes("__PRODUCT_DETAIL_APP_INITIAL_STATE__")
+    ) {
+      indexApp = i;
     }
-    let veri = komutArr[indexApp].innerHTML.replace("window.__PRODUCT_DETAIL_APP_INITIAL_STATE__=", "").replace("};window.TYPageName=\"product_detail\";window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'reco'});window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'cross'});window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'reviews'});window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'product_desc'});window.dispatchDREvent&&window.dispatchDREvent({type:'done',key:'pdp'});window.dispatchDREvent&&window.dispatchDREvent({type:'replaceWithSelector',selector:'#product-reviews-dr',replaceValue:\"<h2 class='title' id='product-reviews-dr'>\"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.brand.name+\" \"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.name+\" Yorumları</h2>\"});window.dispatchDREvent&&window.dispatchDREvent({type:'replaceWithSelector',selector:'#detail-attributes-dr',replaceValue:\"<h2 class='detail-attributes-title' id='detail-attributes-dr'>\"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.brand.name+\" \"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.name+\" Özellikleri</h2>\"});window.dispatchDREvent&&window.dispatchDREvent({type:'replaceWithSelector',selector:'#other-merchants-dr',replaceValue:\"<div class='pr-omc-tl title' id='other-merchants-dr'>\"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.brand.name+\" \"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.name+\" Fiyatları</div>\"});", "}");
+  }
+  let veri = komutArr[indexApp].innerHTML
+    .replace("window.__PRODUCT_DETAIL_APP_INITIAL_STATE__=", "")
+    .replace(
+      "};window.TYPageName=\"product_detail\";window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'reco'});window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'cross'});window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'reviews'});window.dispatchDREvent&&window.dispatchDREvent({type:'wait',key:'product_desc'});window.dispatchDREvent&&window.dispatchDREvent({type:'done',key:'pdp'});window.dispatchDREvent&&window.dispatchDREvent({type:'replaceWithSelector',selector:'#product-reviews-dr',replaceValue:\"<h2 class='title' id='product-reviews-dr'>\"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.brand.name+\" \"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.name+\" Yorumları</h2>\"});window.dispatchDREvent&&window.dispatchDREvent({type:'replaceWithSelector',selector:'#detail-attributes-dr',replaceValue:\"<h2 class='detail-attributes-title' id='detail-attributes-dr'>\"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.brand.name+\" \"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.name+\" Özellikleri</h2>\"});window.dispatchDREvent&&window.dispatchDREvent({type:'replaceWithSelector',selector:'#other-merchants-dr',replaceValue:\"<div class='pr-omc-tl title' id='other-merchants-dr'>\"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.brand.name+\" \"+window.__PRODUCT_DETAIL_APP_INITIAL_STATE__.product.name+\" Fiyatları</div>\"});",
+      "}"
+    );
 
-    let objVariants = JSON.parse(veri).product.allVariants;
-    var keys = Object.keys(objVariants);
-    let barkodlar = "";
-    for (i = 0; i < keys.length; i++) {
+  let jsonVeri = JSON.parse(veri).product;
 
-        barkodlar += `<div id='copy1'> Barkod: <a class='barcode' style="cursor:pointer" >${objVariants[i].barcode}</a></div><br><hr>`;
-        chrome.runtime.sendMessage([document.title, barkodlar]);
-    }
-
+  let objVariants = JSON.parse(veri).product.allVariants;
+  var keys = Object.keys(objVariants);
+  let barkodlar = "";
+  let newReLoadButton = "";
+  for (i = 0; i < keys.length; i++) {
+    barkodlar += `<div id='copy1'> Barkod: <a class='barcode' style="cursor:pointer" >${objVariants[i].barcode}</a></div><br><hr>`;
+    chrome.runtime.sendMessage([document.title, barkodlar, objVariants, jsonVeri]);
+  }
 }
 kods();
-
